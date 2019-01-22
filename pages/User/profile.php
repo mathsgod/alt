1c7f87852d3dcb459ee7ad272d90dac286115288
Raymond Chong <mathsgod@yahoo.com>
2019-01-22 16:19:28 +0800

    fido2 move to profile

--- pages/User/profile.php
+++ pages/User/profile.php
@@ -1,4 +1,5 @@
 <?php
+use R\WebAuthn;
 
 class User_profile extends ALT\Page
 {
@@ -15,6 +16,21 @@ class User_profile extends ALT\Page
             App::Msg("User information updated");
         }
         App::Redirect("User/profile");
+    }
+
+    public function postFido2()
+    {
+        $weba = new WebAuthn($_SERVER['HTTP_HOST']);
+        $user = $this->app->user;
+        $user->credential = json_encode($weba->register($_POST));
+        $user->save();
+        return ["code" => 200, "username" => $user->username];
+    }
+
+    public function getCredential()
+    {
+        $weba = new WebAuthn($_SERVER['HTTP_HOST']);
+        return ["data" => $weba->prepare_challenge_for_registration($this->app->user->username, $this->app->user->user_id . "")];
     }
 
     public function get()
