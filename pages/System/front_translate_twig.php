6fde57ea39ef2d681f5b0253b8bf4b491c550401
Raymond Chong <mathsgod@yahoo.com>
2019-08-28 17:08:03 +0800

    update

--- pages/System/front_translate_twig.php
+++ pages/System/front_translate_twig.php
@@ -1,17 +1,175 @@
 <?php
+
 use SplFileInfo;
 use RecursiveIteratorIterator;
-use App\Config;
-use App;
 use My\TreeView;
-use WebClient;
 
 require_once(__dir__ . "/str_chinese.php");
 require_once(__dir__ . "/msgfmt-functions.php");
 //require_once (__dir__ . "/accesstokenauthentication.php");
 
+class PO
+{
+
+    public $parser;
+    public $file;
+    public function __construct(string $file)
+    {
+        if (!file_exists($file)) {
+            throw new Exception("$file not found");
+        }
+        $this->file = $file;
+        $this->parser = \Sepia\PoParser::parseFile($file, ["multiline-glue" => ""]);
+    }
+
+    public function setEntry($name, $value)
+    {
+        $previous = $this->getEntry($name);
+
+        $this->parser->setEntry($name, [
+            "msgid" => $name,
+            "msgstr" => $value,
+            "previous" => $previous
+        ]);
+    }
+
+    public function getEntry($name)
+    {
+        $entries = $this->parser->getEntries();
+        return $entries[$name];
+    }
+
+    public function save()
+    {
+        return $this->parser->writeFile($this->file);
+    }
+    public function getEntries()
+    {
+        return $this->parser->getEntries();
+    }
+
+    public function toMoFile(string $mo)
+    {
+        $hash = parse_po_file($this->file);
+        if ($hash === false) {
+            throw new Exception("Error reading $this->file, aborted");
+        }
+        write_mo_file($hash, $mo);
+    }
+}
+
 class System_front_translate_twig extends \ALT\Page
 {
+    public function rglob($pattern, $flags = 0)
+    {
+        $files = glob($pattern, $flags);
+        foreach (glob(dirname($pattern) . '/*', GLOB_ONLYDIR | GLOB_NOSORT) as $dir) {
+            $files = array_merge($files, $this->rglob($dir . '/' . basename($pattern), $flags));
+        }
+        return $files;
+    }
+
+    public function generatePO()
+    {
+        $root = $this->getRootPath();
+        $fpath = $this->frontPath();
+
+        $twigs = [];
+        foreach ($this->rglob($fpath . "/*.twig") as $twig) {
+            $twigs[] = substr($twig, strlen($fpath));
+        }
+
+        foreach ($this->getLang() as $lang) {
+            foreach ($twigs as $twig) {
+                $fi = pathinfo($twig);
+
+                $po = $root . "/locale/$lang/LC_MESSAGES/" . $fi["dirname"] . "/" . $fi["filename"] . ".po";
+
+                if (!file_exists($po)) {
+                    mkdir(dirname($po), 0777, true);
+                    file_put_contents($po, "");
+                }
+            }
+        }
+    }
+
+    public function applyGlobal()
+    {
+        $this->generatePO();
+        $root = $this->getRootPath();
+        $entries = $this->globalData()["entries"];
+
+        try {
+            foreach ($this->getLang() as $lang) {
+
+                foreach ($this->rglob($root . "/locale/$lang/*.po") as $po_file) {
+                    $po = new PO($po_file);
+
+                    foreach ($entries as $entry) {
+                        $po->setEntry($entry["name"], $entry["value"][$lang]);
+                    }
+
+                    $po->save();
+
+                    //complie to mo
+                    $fi = pathinfo($po_file);
+                    $mo = dirname($po_file) . "/" . $fi["filename"] . "-" . time() . ".mo";
+                    $po->toMoFile($mo);
+                }
+            }
+        } catch (Exception $e) {
+            return ["error" => ["message" => $e->getMessage()]];
+        }
+        return ["data" => true];
+    }
+
+
+    public function upldateGlobal()
+    {
+        $frontPage = new SplFileInfo($this->frontPath());
+        $basePath = new SplFileInfo($frontPage->getPath());
+        foreach ($this->getLang() as $lang) {
+            $data = [];
+            foreach ($_POST["data"] as $d) {
+                $data[$d["name"]] = $d["value"][$lang];
+            }
+
+            $file = $basePath . "/locale/$lang/global.json";
+            file_put_contents($file, json_encode($data, JSON_UNESCAPED_UNICODE));
+        }
+    }
+
+    public function globalData()
+    {
+        $data = [];
+        $frontPage = new SplFileInfo($this->frontPath());
+        $basePath = new SplFileInfo($frontPage->getPath());
+        foreach ($this->getLang() as $lang) {
+            $file = $basePath . "/locale/$lang/global.json";
+            if (!file_exists($file)) {
+                file_put_contents($file, json_encode([]));
+                $data[$lang] = [];
+            } else {
+                $data[$lang] = json_decode(file_get_contents($file), true);
+            }
+        }
+        $ds = [];
+        foreach ($data as $lang => $d) {
+            foreach ($d as $n => $v) {
+                $ds[$n][$lang] = $v;
+            }
+        }
+
+        $data = [];
+        foreach ($ds as $name => $v) {
+            $data[] = ["name" => $name, "value" => $v];
+        }
+
+        return [
+            "entries" => $data
+        ];
+    }
+
     public function post()
     {
         $post = json_decode(file_get_contents("php://input"), true);
@@ -36,7 +194,6 @@ class System_front_translate_twig extends \ALT\Page
 
             if (!file_exists($po_file = $basePath . "/locale/$lang/LC_MESSAGES/" . $fi["dirname"] . "/" . $fi["filename"] . ".po")) {
                 mkdir(dirname($po_file), 0777, true);
-
             } else {
                 unlink($po_file);
             }
@@ -55,7 +212,7 @@ class System_front_translate_twig extends \ALT\Page
             }
 
             $poParser->writeFile($po_file);
-                      
+
             // del all mo
             foreach (glob(dirname($po_file) . "/" . $fi["filename"] . "-*.mo") as $mo_file) {
                 unlink($mo_file);
@@ -218,18 +375,6 @@ class System_front_translate_twig extends \ALT\Page
     public function frontPath()
     {
         return realpath($this->app->root . "/../pages");
-    }
-
-    public function readAllFile($path)
-    {
-        $files = [];
-        $objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path), RecursiveIteratorIterator::SELF_FIRST);
-        foreach ($objects as $name => $object) {
-            if ($object->getExtension() == "twig") {
-                $files[] = $object;
-            }
-        }
-        return $files;
     }
 
     public function renderTree($path, $tree)
